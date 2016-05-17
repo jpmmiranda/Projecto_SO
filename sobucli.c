7a833b1e4f0077f562901e460ce40af3b184f1e6
jpmmiranda <jota_pmm@hotmail.com>
2016-05-18 00:24:08 +0100

    Update

--- sobucli.c
+++ sobucli.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(int args, char* argv[]){
+	int cmp = 0;
 	
-	
+	cmp = strcmp(argv[1],"backup");
+	if(cmp == 0){
+		execlp() // executa um comando para mover os ficheiros para a pasta backup
+	}
+
 }
