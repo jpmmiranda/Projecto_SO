6fba30d40290f61cbabccea601d2b6ca8ee315ed
jpmmiranda <jota_pmm@hotmail.com>
2016-05-18 18:19:03 +0100

    pipes working

--- sobucli.c
+++ sobucli.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+#define TAM 1024
 
 int main(int args, char* argv[]){
-	int cmp = 0;
-	
-	cmp = strcmp(argv[1],"backup");
-	if(cmp == 0){
-		execlp() // executa um comando para mover os ficheiros para a pasta backup
+	int cmp = 0, t, i = 0;
+	char* opcao;
+	char* ficheiro;
+	char buffer[TAM];
+	mkfifo("/tmp/sobucli_fifo",0666);
+
+	if(args == 3){
+		int fs = open("/tmp/sobusrv_fifo",O_WRONLY);
+		int fc = open("/tmp/sobucli_fifo",O_RDONLY);
+		opcao = strdup(argv[1]);
+		ficheiro = strdup(argv[2]);
+		
+		strcat(opcao," ");
+		strcat(opcao, ficheiro);
+		t = strlen(opcao);
+		if(cmp == 0){
+			write(fs, opcao, t);
+		}
+
+		while((t=read(fc,buffer,TAM)) > 0){
+        	write(1,buffer,t);
+		}			
+		close(fc);
+		close(fs);
+	}
+
+	else if(args<3){
+		printf("Argumentos insuficientes para a execução do programa.\n");
+	}
+	else if(args>3){
+		printf("Demasiados argumentos para a execução do programa.\n");
 	}
 
 }
--- sobusrv.c
+++ sobusrv.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+#define TAM 1024
 
 int main(int args, char* argv[]){
+	int t;
+	char buffer[TAM];
+	printf("\033c");
 	printf("A aguardar...\n");
+	mkfifo("/tmp/sobusrv_fifo",0666);
+
+	int fs = open("/tmp/sobusrv_fifo", O_RDONLY);
+    int fc = open("/tmp/sobucli_fifo", O_WRONLY);
 	
+	while((t=read(fs,buffer,TAM)) > 0){
+        write(1,buffer,t);
+        strcat(buffer,"-> Efectuado com sucesso.");
+        write(fc,buffer , t+26);
+	}
+    
+    close(fs);
+    close(fc);
+
+    return 0;
+
 }
