3f3833ffc34380f0099eb646e211ec23605d676b
jpmmiranda <jota_pmm@hotmail.com>
2016-05-20 03:34:53 +0100

    Backup working

--- sobusrv.c
+++ sobusrv.c
@@ -8,34 +8,120 @@
 
 #define TAM 1024
 
-int backup(char* ficheiro, )
+int backup(char* ficheiro, char* directoriaData, char* directoriaMetadata){
+	int f1,f2, sucesso, bytes;
+	int p[2];
+	char buffer[TAM];
+	char *ficheiroZip = strdup(ficheiro); strcat(ficheiroZip,".gz");
+
+	f1 = fork();
+	if(f1==0){
+		printf("Processo filho a executar o backup..\n");
+		if(execlp("gzip","gzip", ficheiro, NULL)==-1){
+			perror("Gzip");
+			return -1;
+		}
+	}
+	else{
+		wait();
+		sucesso = pipe(p);
+		f2 = fork();
+		if(f2==0 && sucesso == 0){
+			dup2(p[1],1);
+			close(p[0]);
+			if(execlp("sha1sum","sha1sum", ficheiroZip, NULL)==-1){
+				perror("sha1sum");
+				return -1;
+			}
+			close(p[1]);
+		}
+		dup2(p[0],p[1]);
+		close(p[1]);
+		bytes = read(p[0], buffer, TAM);
+		close(p[0]);
+	}
+	
+	char *nomeSum = strtok(buffer, " ");
+	char* nomeData = (char*)malloc(sizeof(directoriaData)+sizeof(nomeSum));
+	strcat(nomeData,directoriaData);
+	strcat(nomeData, nomeSum);
+	if(fork() == 0){
+		if(fork() == 0){
+			if(execlp("mv","mv",ficheiroZip, nomeSum, NULL)==-1){
+			 perror("Renomear o ficheiro");
+			 return -1;
+			}
+		}
+		wait();
+		if(execlp("mv","mv",nomeSum, directoriaData, NULL)==-1){
+			perror("Mover para data"); 
+			return -1;
+		}
+	}
+	else{
+		wait();
+		if(fork()==0){
+			if(execlp("ln", "ln", "-s", nomeData, ficheiro, NULL)==-1){
+				perror("Criar link");	
+				return -1;
+			}
+		}
+		else{
+			wait();
+			if(fork()==0){
+				if(execlp("mv","mv",ficheiro, directoriaMetadata, NULL)==-1){
+				perror("Mover para metadata");
+				return -1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+
+int restore(char* ficheiro, char* directoriaData, char* directoriaMetadata){
+	
+}
+
+
+
 
 
 
 int main(int args, char* argv[]){
 	int t, resultado;
-	char buffer[TAM], directoria[TAM], username[TAM], opcao[TAM], ficheiro[TAM];
+	char buffer[TAM], directoriaData[TAM], directoriaMetadata[TAM], username[TAM], opcao[TAM], ficheiro[TAM];
 	printf("\033c");
 	printf("A aguardar...\n");
 	getlogin_r(username,TAM);
-	sprintf(directoria,"/home/%s/.Backup/",username);
+	sprintf(directoriaData,"/home/%s/.Backup/data/",username);
+	sprintf(directoriaMetadata,"/home/%s/.Backup/metadata/",username);
 	mkfifo("/tmp/sobusrv_fifo",0666);
 
-	int fs = open("/tmp/sobusrv_fifo", O_RDONLY);
-	
-	while((t=read(fs,buffer,TAM)) > 0){
-        write(1,buffer,t);
-        sscanf(buffer,"%s %s", opcao, ficheiro);
-        printf("\n");
-        if(strcmp(opcao, "backup")==0){
-        	resultado = backup(ficheiro);
-		}
-
-
-
+	int fs = open("/tmp/sobusrv_fifo", O_RDWR);
+	while(1){
+		resultado = 1;
+		while((t=read(fs,buffer,TAM)) >= 0){
+	        write(1,buffer,t);
+	        sscanf(buffer,"%s %s", opcao, ficheiro); strcat(opcao,"\0"); strcat(ficheiro,"\0");
+	        printf("\n");
+	        if(strcmp(opcao, "backup")==0){
+	        	resultado = backup(ficheiro, directoriaData, directoriaMetadata);
+	        	if(resultado == 0){
+	        		printf("Backup executado com sucesso.\n");
+	        	}
+			}
+			else if(strcmp(opcao, "restore") == 0){
+				resultado = restore(ficheiro, directoriaMetadata);
+				if(resultado == 0){
+	        		printf("Restore executado com sucesso.\n");
+	        	}
+			}
+		} 
 	}
-    
-    close(fs);
 
     return 0;
 
